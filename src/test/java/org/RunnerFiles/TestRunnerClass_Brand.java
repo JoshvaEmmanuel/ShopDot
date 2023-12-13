bd912a9cf841a04cbb38066db85f57987fb91c93
Jeeva <jeevakrishan96@gmail.com>
2023-12-13 20:10:42 +0530

    refactor

--- src/test/java/org/RunnerFiles/TestRunnerClass_Brand.java
+++ src/test/java/org/RunnerFiles/TestRunnerClass_Brand.java
@@ -6,10 +6,10 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features ="src/test/java/org/featurefies/brandPages", glue = "org.setpdefinition",
+@CucumberOptions(features ="src/test/java/org/featurefies/brandPages", glue ={"org.setpdefinition","CommonHooks"},
 monochrome = true,dryRun = false,
 plugin ={"pretty","html:target/Cucumber-HTML-report/report.html", "junit:target/Cucumber-junit/junitreport.xml", "json:target/Cucumber-Jason-report/jsonreport.json"}
-,tags = "@IP003")
+,tags = "@brandprofile")
 
 
 public class TestRunnerClass_Brand {
