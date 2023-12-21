2143dca897b5a228bd242a1f25e38dd0a4607862
Jeeva <jeevakrishan96@gmail.com>
2023-12-21 13:48:10 +0530

    fix

--- src/test/java/org/setpdefinition/CommonHooks.java
+++ src/test/java/org/setpdefinition/CommonHooks.java
@@ -1,6 +1,5 @@
 package org.setpdefinition;
 
-import io.cucumber.core.api.Scenario;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import org.Base.BaseClase;
@@ -8,7 +7,6 @@ import org.Base.BaseClase;
 
 
 public class CommonHooks  extends BaseClase {
-    Scenario scenario;
 
     // Setup method - runs before each scenario
     @Before
